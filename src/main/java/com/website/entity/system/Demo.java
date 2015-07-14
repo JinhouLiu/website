25c6ccc628227dd803c78c2e1c729a72d2dc6845
刘锦厚 <www.975764577@qq.com>
2015-07-14 09:53:38 +0800

    实现使用Bcrypt算法对数据路密码进行加密

---
+++ src/main/java/com/website/entity/system/Demo.java
@@ -0,0 +1,21 @@
+package com.website.entity.system;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class Demo {
+public static  void   main(String[]  args)
+{
+	
+	int i = 0;
+	while (i < 10) {
+		String password = "ljh8888";
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+ 
+		System.out.println(hashedPassword);
+		i++;
+	}
+
+}	
+	
+}
